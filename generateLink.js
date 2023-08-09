3b123405b2cf77299ca1822f44f70ce9e1ad5147
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2023-08-10 02:37:24 +0800

    Update and rename generateLink.js to generateLink.html

--- generateLink.js
+++
@@ -1,9 +0,0 @@
-function generateLink(phoneNumber, text){
-  return "https://wa.me/"+phoneNumber.toString()+"?text="+text;
-}
-function addNextLine(){
-  return '%0a';
-}
-function addNextLine(){
-  return '+';
-}
