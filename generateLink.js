31b21e77293b4a6c602499d2bbbdc606ffbf38a3
khoo2002 <50482835+khoo2002@users.noreply.github.com>
2023-08-10 02:34:51 +0800

    Create generateLink.js

---
+++ generateLink.js
@@ -0,0 +1,9 @@
+function generateLink(phoneNumber, text){
+  return "https://wa.me/"+phoneNumber.toString()+"?text="+text;
+}
+function addNextLine(){
+  return '%0a';
+}
+function addNextLine(){
+  return '+';
+}
